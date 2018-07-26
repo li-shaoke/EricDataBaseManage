b358e9330f5fbe33ed3292c691c8505061f78d2a
Eric <eric_li@live.cn>
2018-07-26 13:36:47 +0800

    rm wrong server string

---
+++ EricDataBaseManage/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EricDataBaseManage.MvcApplication" Language="C#" %>
---
+++ EricDataBaseManage/obj/Release/Package/PackageTmp/Scripts/tree_data2.js
@@ -0,0 +1,21 @@
+[{"id":1,
+    "text": "tccline",
+    "iconCls": "icon-database",
+	"children":[{
+	    "id": 121,
+	    "text": "Intel"
+	}, {
+	    "id": 122,
+	    "text": "Java"
+	}, {
+	    "id": 123,
+	    "text": "Microsoft Office"
+	}, {
+	    "id": 124,
+	    "text": "Games",
+	    "iconCls": "icon-table",
+	    "attributes": {
+	        "url": "/Home/table",
+	        "price": 100
+	    }
+	}]}]
